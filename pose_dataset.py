ccabad5f2bad2d1a7b3679f8fc74597368e8dbac
YufeiZheng <2415571409@qq.com>
2020-03-01 16:59:36 +0800

    Add files via upload

---
+++ pose_dataset.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Yasunori Kudo
+
+import copy
+import os, sys
+import pickle
+
+import chainer
+import numpy as np
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from . import pose_dataset_base
+
+# Joints in H3.6M -- data has 32 joints,
+# but only 17 that move; these are the indices.
+H36M_NAMES = [''] * 32
+H36M_NAMES[0] = 'Hip'
+H36M_NAMES[1] = 'RHip'
+H36M_NAMES[2] = 'RKnee'
+H36M_NAMES[3] = 'RFoot'
+H36M_NAMES[6] = 'LHip'
+H36M_NAMES[7] = 'LKnee'
+H36M_NAMES[8] = 'LFoot'
+H36M_NAMES[12] = 'Spine' # 脊椎
+H36M_NAMES[13] = 'Thorax' # 胸部
+H36M_NAMES[14] = 'Neck/Nose'
+H36M_NAMES[15] = 'Head'
+H36M_NAMES[17] = 'LShoulder'
+H36M_NAMES[18] = 'LElbow' # 肘
+H36M_NAMES[19] = 'LWrist' # 手腕
+H36M_NAMES[25] = 'RShoulder'
+H36M_NAMES[26] = 'RElbow'
+H36M_NAMES[27] = 'RWrist'
+
+
+##################################################################
+# 用于返回动作名
+ACTION_NAMES = [''] * 15
+ACTION_NAMES[0] = 'Directions'
+ACTION_NAMES[1] = 'Discussion'
+ACTION_NAMES[2] = 'Eating'
+ACTION_NAMES[3] = 'Greeting'
+ACTION_NAMES[4] = 'Phoning'
+ACTION_NAMES[5] = 'Photo'
+ACTION_NAMES[6] = 'Posing'
+ACTION_NAMES[7] = 'Purchases'
+ACTION_NAMES[8] = 'Sitting'
+ACTION_NAMES[9] = 'SittingDown'
+ACTION_NAMES[10] = 'Smoking'
+ACTION_NAMES[11] = 'Waiting'
+ACTION_NAMES[12] = 'WalkDog'
+ACTION_NAMES[13] = 'Walking'
+ACTION_NAMES[14] = 'WalkTogether'
+##################################################################
+
+
+def project_point_radial(P, R, T, f, c, k, p):
+    """
+    Project points from 3d to 2d using camera parameters
+    including radial and tangential distortion
+    Args
+    P: Nx3 points in world coordinates 世界坐标系的关节坐标点
+    R: 3x3 Camera rotation matrix 旋转矩阵
+    T: 3x1 Camera translation parameters 摄像机转换参数
+    f: (scalar) Camera focal length 相机焦距
+    c: 2x1 Camera center 相机中心
+    k: 3x1 Camera radial distortion coefficients 相机径向畸变系数
+    p: 2x1 Camera tangential distortion coefficients 相机切向畸变系数
+    Returns
+    Proj: Nx2 points in pixel space
+    D: 1xN depth of each point in camera space
+    radial: 1xN radial distortion per point
+    tan: 1xN tangential distortion per point
+    r2: 1xN squared radius of the projected points before distortion
+    """
+
+    # P is a matrix of 3-dimensional points
+    assert len(P.shape) == 2
+    assert P.shape[1] == 3
+
+    N = P.shape[0]
+    # R矩阵乘(P的转置-T)
+    #########################################################################
+    X = R.dot(P.T - T)  # rotate and translate // dot矩阵乘法 .T转置
+    #########################################################################
+    # XX = 用xy坐标的矩阵/z坐标的矩阵
+    XX = X[:2, :] / X[2, :] # X[:2, :] // 列全取 行取两行
+    # r2 = x/z得到的矩阵的平方 + y/z得到的矩阵的平方
+    r2 = XX[0, :] ** 2 + XX[1, :] ** 2 # ** 2表示矩阵乘方
+
+    # np.tile k行向复制N倍，纵向复制1倍
+    radial = 1 + np.einsum(
+        'ij,ij->j', np.tile(k, (1, N)), np.array([r2, r2 ** 2, r2 ** 3]))
+    #XX乘以相机径向畸变系数
+    tan = p[0] * XX[1, :] + p[1] * XX[0, :]
+
+    # np.outer para1的第一个元素与para2的每一个元素分别相乘得到第一行
+    #                 第二个元素与para2的每一个元素分别相乘得到第二行
+    XXX = XX * np.tile(radial + tan, (2, 1)) + \
+        np.outer(np.array([p[1], p[0]]).reshape(-1), r2)
+
+    Proj = (f * XXX) + c
+    Proj = Proj.T
+
+    D = X[2,]
+
+    return Proj, D, radial, tan, r2
+
+
+class H36M(pose_dataset_base.PoseDatasetBase):
+
+    def __init__(self, action='all', length=1,
+                 train=True, use_sh_detection=False):
+        if train:
+            subjects = ['S1', 'S5', 'S6', 'S7', 'S8']
+        else:
+            subjects = ['S9', 'S11']
+        # subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
+        if not os.path.exists('data/h36m'):
+            os.mkdir('data/h36m')
+
+        # 3D点
+        if not os.path.exists('data/h36m/points_3d.pkl'):
+            print('Downloading 3D points in Human3.6M dataset.')
+            os.system('wget --no-check-certificate "https://onedriv' + \
+                'e.live.com/download?cid=B08D60FE71FF90FD&resid=B08' + \
+                'D60FE71FF90FD%2118616&authkey=AFIfEB6VYEZnhlE" -O ' + \
+                'data/h36m/points_3d.pkl')
+        with open('data/h36m/points_3d.pkl', 'rb') as f:
+            p3d = pickle.load(f)
+        # 相机参数
+        if not os.path.exists('data/h36m/cameras.pkl'):
+            print('Downloading camera parameters.')
+            os.system('wget --no-check-certificate "https://onedriv' + \
+                'e.live.com/download?cid=B08D60FE71FF90FD&resid=B08' + \
+                'D60FE71FF90FD%2118615&authkey=AEUoi3s16rBTFRA" -O ' + \
+                'data/h36m/cameras.pkl')
+
+        with open('data/h36m/cameras.pkl', 'rb') as f:
+            cams = pickle.load(f)
+        # StackedHourglass检测结果
+        if use_sh_detection:
+            if not os.path.exists('data/h36m/sh_detect_2d.pkl'):
+                print('Downloading detected 2D points by Stacked Hourglass.')
+                os.system('wget --no-check-certificate "https://onedriv' + \
+                    'e.live.com/download?cid=B08D60FE71FF90FD&resid=B08' + \
+                    'D60FE71FF90FD%2118619&authkey=AMBf6RPcWQgjsh0" -O ' + \
+                    'data/h36m/sh_detect_2d.pkl')
+            with open('data/h36m/sh_detect_2d.pkl', 'rb') as f:
+                p2d_sh = pickle.load(f)
+
+        with open('data/actions.txt') as f:
+            actions_all = f.read().split('\n')[:-1]
+        if action == 'all':
+            actions = actions_all
+        elif action in actions_all:
+            actions = [action]
+        else:
+            raise Exception('Invalid action.')
+
+        # 要使用的关节位置索引(17点)
+        # 坐标的排布是按照 x1,y1,z1 ... xn, yn, zn 进行排布的
+        dim_to_use_x = np.where(np.array([x != '' for x in H36M_NAMES]))[0] * 3
+        dim_to_use_y = dim_to_use_x + 1
+        dim_to_use_z = dim_to_use_x + 2
+        dim_to_use = np.array(
+            [dim_to_use_x, dim_to_use_y, dim_to_use_z]).T.flatten()
+        self.N = len(dim_to_use_x)
+
+        p3d = copy.deepcopy(p3d)
+        self.data_list = []
+        for s in subjects:
+            for action_name in actions:
+                def search(a):
+                    fs = list(filter(
+                        lambda x: x.split()[0] == a, p3d[s].keys()))
+                    return fs
+
+                files = []
+                files += search(action_name)
+                # 'Photo' is 'TakingPhoto' in S1
+                if action_name == 'Photo':
+                    files += search('TakingPhoto')
+                # 'WalkDog' is 'WalkingDog' in S1
+                if action_name == 'WalkDog':
+                    files += search('WalkingDog')
+                for file_name in files:
+                    p3d[s][file_name] = p3d[s][file_name][:, dim_to_use]
+                    L = p3d[s][file_name].shape[0]
+                    for cam_name in cams[s].keys():
+                        if not (cam_name == '54138969' and s == 'S11' \
+                                and action_name == 'Directions'):
+                            # 50Hz -> 10Hz
+                            for start_pos in range(0, L - length + 1, 5):
+                                info = {'subject': s,
+                                        'action_name': action_name,
+                                        'start_pos': start_pos,
+                                        'length': length,
+                                        'cam_name': cam_name,
+                                        'file_name': file_name}
+                                self.data_list.append(info)
+        self.p3d = p3d
+        self.cams = cams
+        self.train = train
+        self.use_sh_detection = use_sh_detection
+        if use_sh_detection:
+            self.p2d_sh = p2d_sh
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def get_example(self, i):
+        info = self.data_list[i]
+        subject = info['subject']
+        start_pos = info['start_pos']
+        length = info['length']
+        cam_name = info['cam_name']
+        file_name = info['file_name']
+
+        poses_xyz = self.p3d[subject][file_name][start_pos:start_pos + length]
+        params = self.cams[subject][cam_name]
+        if self.use_sh_detection:
+            if 'TakingPhoto' in file_name:
+                file_name = file_name.replace('TakingPhoto', 'Photo')
+            if 'WalkingDog' in file_name:
+                file_name = file_name.replace('WalkingDog', 'WalkDog')
+            sh_detect_xy = self.p2d_sh[subject][file_name]
+            sh_detect_xy = sh_detect_xy[cam_name][start_pos:start_pos+length] # 读取数据
+
+        # 上面有 if use_sh的这一步操作，不用sh_detection也要加上这一步操作
+        ######################################################################
+        if 'TakingPhoto' in file_name:
+            file_name = file_name.replace('TakingPhoto', 'Photo')
+        if 'WalkingDog' in file_name:
+            file_name = file_name.replace('WalkingDog', 'WalkDog')
+        ######################################################################
+
+        # 来自相机位置的平行投影
+        P = poses_xyz.reshape(-1, 3)
+        X = params['R'].dot(P.T).T
+        R_inv = np.linalg.inv(params['R'])
+        X = X.reshape(-1, self.N * 3)  # shape=(length, 3*n_joints)
+
+        # Normalization of 3d points.
+        X, scale = self._normalize_3d(X) # X先变换 再normalize 两个视角的同一个3D scale也不同
+        X = X.astype(np.float32)
+        R_inv = R_inv.astype(np.float32)
+        scale = scale.astype(np.float32)
+
+        #####################################################################
+        # a确定为动作下标
+        a = 0
+        while ACTION_NAMES[a] != file_name.split()[0]:
+            a = a + 1
+
+        # 在return处多返回一个a
+        ######################################################################
+
+        if self.use_sh_detection:
+            # 通过StackedHourglass检测到的Pose的正规化
+            sh_detect_xy = self._normalize_2d(sh_detect_xy)
+            sh_detect_xy = sh_detect_xy.astype(np.float32)
+            # return sh_detect_xy, X, scale
+            return sh_detect_xy, X, scale, a, R_inv
+        else:
+            # 使用相机参数投影到图像上
+            proj = project_point_radial(P, **params)[0]
+            proj = proj.reshape(-1, self.N * 2)  # shape=(length, 2*n_joints)
+            proj = self._normalize_2d(proj)
+            proj = proj.astype(np.float32)
+            return proj, X, scale, a, R_inv
+
+
+class MPII(chainer.dataset.DatasetMixin):
+    def __init__(self, train=True, use_sh_detection=False):
+        if use_sh_detection:
+            raise NotImplementedError
+        else:
+            self.poses = np.load('data/mpii_poses.npy')
+
+        np.random.seed(100)
+        perm = np.random.permutation(len(self.poses))
+        if train:
+            self.poses = self.poses[perm[:int(len(self.poses)*0.9)]]
+        else:
+            self.poses = self.poses[perm[int(len(self.poses)*0.9):]]
+
+    def __len__(self):
+        return self.poses.shape[0]
+
+    def get_example(self, i):
+        mpii_poses = self.poses[i:i+1]
+        # hip(0)と各関節点の距離の平均値が1になるようにスケール
+        xs = mpii_poses.T[0::2] - mpii_poses.T[0]
+        ys = mpii_poses.T[1::2] - mpii_poses.T[1]
+        # hip(0)が原点になるようにシフト
+        mpii_poses = mpii_poses.T / np.sqrt(xs[1:]**2 + ys[1:]**2).mean(axis=0)
+        mpii_poses[0::2] -= mpii_poses[0]
+        mpii_poses[1::2] -= mpii_poses[1]
+        mpii_poses = mpii_poses.T.astype(np.float32)[None]
+
+        # dummy_X = np.zeros((1, 1, 17*3), dtype=np.float32)
+        # dummy_X[0, 0, 0::3] = mpii_poses[0, 0, 0::2]
+        # dummy_X[0, 0, 1::3] = mpii_poses[0, 0, 1::2]
+        dummy_X = np.zeros((1, 17*3), dtype=np.float32)
+        dummy_X[0, 0::3] = mpii_poses[0, 0, 0::2]
+        dummy_X[0, 1::3] = mpii_poses[0, 0, 1::2]
+
+        dummy_scale = np.array([1], dtype=np.float32)
+
+        mpii_1 = np.zeros((1 ,34), dtype=np.float32)
+        mpii_1[0, 0::1] = mpii_poses[0, 0, 0::1]
+
+        return mpii_1, dummy_X, dummy_scale
+a = []
+a.
---
+++ pose_dataset_base.py
@@ -0,0 +1,42 @@
+import chainer
+import numpy as np
+
+
+class Normalization(object):
+    @staticmethod
+    def normalize_3d(pose):
+        # 3Dモデルの正規化
+        # hip(0)と各関節点の距離の平均値が1になるようにスケール
+        xs = pose.T[0::3] - pose.T[0] # 17
+        ys = pose.T[1::3] - pose.T[1] # 17
+        zs = pose.T[2::3] - pose.T[2] # 17
+        ls = np.sqrt(xs[1:] ** 2 + ys[1:] ** 2 + zs[1:] ** 2)  # 原点からの距離 17个关节点到中心点的距离
+        scale = ls.mean(axis=0) # 距离的平均
+        pose = pose.T / scale # 缩放 使距离的平均值为1
+        # hip(0)が原点になるようにシフト
+        pose[0::3] -= pose[0].copy()
+        pose[1::3] -= pose[1].copy()
+        pose[2::3] -= pose[2].copy()
+        return pose.T, scale
+
+    @staticmethod
+    def normalize_2d(pose):
+        # 2DPoseの正規化
+        # hip(0)と各関節点の距離の平均値が1になるようにスケール
+        xs = pose.T[0::2] - pose.T[0]
+        ys = pose.T[1::2] - pose.T[1]
+        pose = pose.T / np.sqrt(xs[1:] ** 2 + ys[1:] ** 2).mean(axis=0)
+        # hip(0)が原点になるようにシフト
+        mu_x = pose[0].copy()
+        mu_y = pose[1].copy()
+        pose[0::2] -= mu_x
+        pose[1::2] -= mu_y
+        return pose.T
+
+
+class PoseDatasetBase(chainer.dataset.DatasetMixin):
+    def _normalize_3d(self, pose):
+        return Normalization.normalize_3d(pose)
+
+    def _normalize_2d(self, pose):
+        return Normalization.normalize_2d(pose)
